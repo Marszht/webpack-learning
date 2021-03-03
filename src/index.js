0e051deb9e7babdbab34358cc0df90dacf4ecd47
Mars <10462097992@qq.com>
2021-03-03 19:59:41 +0800

    <F>

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-// import "@babel/polyfill"; // 会再 全局注入， 如果不希望全局注入需要  transform-runtime 
+// import "@babel/polyfill"; // 会再 全局注入， 如果不希望全局注入需要  transform-runtime
 // polyfill会污染 全局
 // 闭包注入
 
 // import "core-js/stable";
 // import "regenerator-runtime/runtime";
 
-
-
 // // 模块打包工具
 // import Header from './header';
 // import Content from './content';
 // import SiderBar from './siderBar';
 // import createAvatar from './createAvatar';
-import zhouJ from './assets/LMY22301333226.jpg';
+import zhouJ from "./assets/LMY22301333226.jpg";
 // const ZhouJ = require('./assets/LMY22301333226.jpg')
 // const avatar = require('./assets/avatar.jpg')
 // const pushUp = require('./assets/pushUp.jpg')
-
-
 
 // // 会造成这个样式全局引用 所以引出 css-module
 
 import style from "./index.scss";
 // import "./style.css";
-console.log({style})
+console.log({ style });
 // const img = new Image();
 // img.src = zhouJ;
 // img.classList.add(style.avatar);
@@ -31,19 +27,17 @@ console.log({style})
 // const root = document.getElementById('root');
 // root.appendChild(img);
 
-// // 打包图片 以及 css 的打包  
+// // 打包图片 以及 css 的打包
 // // 其中主要包括  url-loader file-loader
-// // postcss-loader, sass-loader, css-modules 
+// // postcss-loader, sass-loader, css-modules
 // // css-loader, style-loader,
 // createAvatar();
 // console.log("ddd");
 
-
-// // 基础打包， 打包 js 文件 
+// // 基础打包， 打包 js 文件
 // // new Header();
 // // new Content();
 // // new SiderBar();
-
 
 // // 打包 字体文件
 
@@ -52,15 +46,13 @@ console.log({style})
 // const root = document.getElementById('root');
 // console.log("sdsfd")
 
-
 // root.innerHTML = '<i class=" icon-weixinzhifu iconfont ">Hello webpack !!</i>';
 
-// webpack-dev-server  // 会把打包的目录 放到内存中 从而提高 打包速度 
+// webpack-dev-server  // 会把打包的目录 放到内存中 从而提高 打包速度
 // console.log("HELLO WD i !!!! ");
 
-
 // css-loader 中已经 帮我们自己判断是否需要更新
-// HMR 
+// HMR
 // import style from './index.scss';
 
 // console.log({style});
@@ -101,7 +93,6 @@ console.log({style})
 //   console.log({ item })
 // })
 
-
 // import React, { PureComponent } from 'react';
 
 // import ReactDom from 'react-dom';
@@ -115,14 +106,14 @@ console.log({style})
 //     }
 //   }
 
-  // componentDidMount() {
-  //   // 允许了跨域
-  //   // 本地代理服务器 fideler 待会尝试一下
-  //   axios.get('/react/api/header.json')
-  //   .then(res => {
-  //     console.log({res});
-  //   })
-  // }
+// componentDidMount() {
+//   // 允许了跨域
+//   // 本地代理服务器 fideler 待会尝试一下
+//   axios.get('/react/api/header.json')
+//   .then(res => {
+//     console.log({res});
+//   })
+// }
 
 // onClick = () => {
 
@@ -143,14 +134,15 @@ console.log({style})
 // ReactDom.render(<App />, document.getElementById('root'));
 
 /**
- * tree shaking 
+ * tree shaking
  */
 // tree shaking 去除不需要的 引入
 // 需要配合 sideEffects
 // 以及 压缩 treser
+// 还有字段 usedExports： true当值为true 之后再通过 terser 压缩 会把 没有使用的给删除掉
 // 只支持 es module的引入 只支持静态引入
-import { add } from './math';
-console.log(add(1,4));
+import { add } from "./math";
+console.log(add(1, 4));
 
 /**
  * code splitting 代码分割
@@ -189,15 +181,14 @@ console.log(add(1,4));
 // 1. 同步代码分割 // 在webpack 中 optimization
 // 2.异步代码，无需做任何配置
 
-
 /**
- * 懒加载 Lazy Loading 
+ * 懒加载 Lazy Loading
  * Chunk 是什么
  */
 // 让我们加载更快
 // function getComponent() {
-//   // 异步加载 import 懒加载 
-// //  也可以用 async await 
+//   // 异步加载 import 懒加载
+// //  也可以用 async await
 //   return import(/* webpackChunkName:"lodash"*/'lodash').then(({ default: _ }) => {
 //     var element = document.createElement('div');
 //     element.innerHTML = _.join(['mars', 'zht'], '-')
@@ -222,7 +213,7 @@ console.log(add(1,4));
 // 库代码
 // console.log("work");
 
-// PWA 渐进式网页应用 
+// PWA 渐进式网页应用
 // if ('serviceWorker' in navigator) {
 //   window.addEventListener('load', () => {
 //     navigator.serviceWorker.register('/service-worker.js')
