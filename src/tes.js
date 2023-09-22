e82e06c845c0154253d1b11cc89e0bee27f1e74a
Mars <10462097992@qq.com>
2023-09-22 19:06:30 +0800

    feat: node param

--- src/tes.js
+++ src/tes.js
@@ -1,3 +1,10 @@
-export const a = {
-  name: 'mars'
-}
+// 可以看下 第三方库处理放方式
+
+// console.log(process.argv);
+const reg = /^([^=]+)=([\s\S]*)$/;
+const params = process.argv.slice(2);
+console.log(params);
+console.log(process.env.a);
+// export const a = {
+//   name: 'mars',
+// };
